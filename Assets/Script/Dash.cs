8430a6b48b3c343cce7907aa6ea87706ed5b5f8d
Eva291204 <eva.fricaud@e-artsup.net>
2022-11-30 15:56:59 +0100

    Test+Box collider lvl 1+2, créa lvl 3, créa fin lvl0

--- Assets/Script/Dash.cs
+++ Assets/Script/Dash.cs
@@ -1,21 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    //private bool CanDash = true;
+    private bool DashE;
+    public float DashSpeed;
+    private float StartDashCount;
+    public float DashCount;
+    private int side;
+
+    private Rigidbody2D rb;
+    //private TrailRenderer tr;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-    }
+        //if (Input.GetKeyDown(KeyCode.E) && CanDash)
+        //   {
+        //       StartCoroutine(DashE());
+        //   }
 
-  
-    
+        //if (Dashing)
+        //   {
+        //       return;
+        //   }
+
+        //DASH
+        if (side == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                side = 1;
+
+            }
+
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                side = 2;
+            }
+
+            else
+            {
+                if (DashCount <= 0)
+                {
+                    side = 0;
+                    DashCount = StartDashCount;
+                    rb.velocity = Vector2.zero;
+                    DashCount = Time.deltaTime;
+                }
+
+                if (side == 1)
+                {
+                    rb.velocity = Vector2.left * DashSpeed;
+                }
+                else if (side == 2)
+                {
+                    rb.velocity = Vector2.right * DashSpeed;
+                }
+            }
+        }
+    }
+    //private void FixedUpdate()
+    //{
+    //    //if (Dashing)
+    //    //{ 
+    //    //    return; 
+    //    //}
+    //}
+    //private IEnumerator DashE()
+    //{
+    //    CanDash = false;
+    //    Dashing = true;
+    //    float originalGravity = rb.gravityScale;
+    //   rb.gravityScale = 0f;
+    //    rb.velocity = new Vector2(transform.localScale.x * DashForce, 0f);
+    //    tr.emitting = true;
+    //    yield return new WaitForSeconds(DashTime);
+    //    tr.emitting = false;
+    //    rb.gravityScale = originalGravity;
+    //    Dashing = false;
+    //    yield return new WaitForSeconds(DashWait);
+    //    CanDash = true;
+    //}
+
+
 }
